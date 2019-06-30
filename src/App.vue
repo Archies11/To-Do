765b34a6933660164ef9a428a395aa803680bc0b
Archies11 <iit2018018@iiita.ac.in>
2019-07-01 02:51:01 +0530

    second

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <Todos v-bind:todos="todos_list" v-on:del-todo="deleteItem"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Layout/Header.vue';
+import Todos from './components/Todos.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Todos,
+    Header
+  },
+  data() {
+    return {
+      todos_list: [
+        {
+          id: 1,
+          title: "Todo One",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "Todo Two",
+          completed: true
+        },
+        {
+          id: 3,
+          title: "Todo Three",
+          completed: false
+        }
+      ]
+    }
+  },
+  methods: {
+    deleteItem(id) {
+      this.todos_list = this.todos_list.filter(todo => todo.id!== id);
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
 }
 </style>
